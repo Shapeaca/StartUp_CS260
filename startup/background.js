c6f05f1fffca7789803c7fc4723e4de1a8dcd731
shapeaca <shapeaca101@gmail.com>
2024-03-07 16:29:46 -0700

    CSS - Finished making the background change color dynamically

--- startup/background.js
+++ startup/background.js
@@ -1,11 +1,15 @@
-var deg = 300;
+var deg = 0;
+// document.querySelector("main").style.backgroundImage = `linear-gradient(hsl(${deg}deg 100% 18%), hsl(${deg}deg 100% 50%))`
 
-setInterval(function() {
-    deg =+ 60;
+function changeBackgroundColor() {
+    deg += 1;
     if(deg >= 360) {
         deg = deg - 360;
     }
     document.querySelector("main").style.backgroundImage = `linear-gradient(hsl(${deg}deg 100% 18%), hsl(${deg}deg 100% 50%))`
-}, 1000)
+
+}
+
+setInterval(changeBackgroundColor, 300)
 
 // document.querySelector("main").style.backgroundImage = `linear-gradient(hsl(${deg}deg 100% 18%), hsl(${deg}deg 100% 50%))`
