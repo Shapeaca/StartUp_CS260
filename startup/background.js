fa06f2a837b2dd39218e3507942f59eba2a2c513
shapeaca <shapeaca101@gmail.com>
2024-03-18 16:24:32 -0600

    CSS - Changed Background Colors some

--- startup/background.js
+++ startup/background.js
@@ -6,9 +6,9 @@ function changeBackgroundColor() {
     if(deg >= 360) {
         deg = deg - 360;
     }
-    document.querySelector("nav").style.background = `hsl(${deg}deg 100% 28%)`
-    document.querySelector("main").style.backgroundImage = `linear-gradient(hsl(${deg}deg 100% 18%), hsl(${deg}deg 100% 50%))`
-    document.querySelector("footer").style.background = `hsl(${deg}deg 100% 28%)`
+    document.querySelector("nav").style.background = `hsl(${deg}deg 100% 23%)`
+    document.querySelector("main").style.backgroundImage = `linear-gradient(hsl(${deg}deg 100% 18%), hsl(${deg}deg 100% 40%))`
+    document.querySelector("footer").style.background = `hsl(${deg}deg 100% 23%)`
     //let tempDeg = deg + 120;
     //document.querySelector("body").style.color = `hsl(${tempDeg}deg 100% 50%)` // I don't like this
 }
