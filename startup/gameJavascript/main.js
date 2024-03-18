81a38c51b29625c93b4265a59fdee05ec0abae9c
shapeaca <shapeaca101@gmail.com>
2024-03-18 16:40:02 -0600

    JS - Created a main function that gets called 30 times per second - This is the game loop

---
+++ startup/gameJavascript/main.js
@@ -0,0 +1,15 @@
+/*var i = 0;
+var milicount = 0;*/
+
+function main() {
+/*    i += 1;
+    milicount += 33.333;
+    console.log(i + " : " + milicount);
+    if(i >= 30) {
+        i = 0;
+        milicount = 0;
+    }*/
+
+}
+
+setInterval(main, 33.333);
