5d6ea5655411fc79214f5f2eab825fa4160a0ca6
shapeaca <shapeaca101@gmail.com>
2024-03-30 20:41:05 -0600

    JS - Used sessionStorage to save the background page color between different pages!

--- startup/background.js
+++ startup/background.js
@@ -1,14 +1,23 @@
-var deg = 0;
-// document.querySelector("main").style.backgroundImage = `linear-gradient(hsl(${deg}deg 100% 18%), hsl(${deg}deg 100% 50%))`
+// sessionStorage.setItem("backgroundColor", `0`);
+let deg = Number(sessionStorage.getItem("backgroundColor"));
+if(deg == null) {
+    deg = 0;
+}
+document.querySelector("nav").style.background = `hsl(${deg}deg 100% 23%)`;
+document.querySelector("main").style.backgroundImage = `linear-gradient(hsl(${deg}deg 100% 18%), hsl(${deg}deg 100% 40%))`;
+document.querySelector("footer").style.background = `hsl(${deg}deg 100% 23%)`;
 
 function changeBackgroundColor() {
+    let deg = Number(sessionStorage.getItem("backgroundColor"));
     deg += 1;
     if(deg >= 360) {
         deg = deg - 360;
     }
-    document.querySelector("nav").style.background = `hsl(${deg}deg 100% 23%)`
-    document.querySelector("main").style.backgroundImage = `linear-gradient(hsl(${deg}deg 100% 18%), hsl(${deg}deg 100% 40%))`
-    document.querySelector("footer").style.background = `hsl(${deg}deg 100% 23%)`
+    document.querySelector("nav").style.background = `hsl(${deg}deg 100% 23%)`;
+    document.querySelector("main").style.backgroundImage = `linear-gradient(hsl(${deg}deg 100% 18%), hsl(${deg}deg 100% 40%))`;
+    document.querySelector("footer").style.background = `hsl(${deg}deg 100% 23%)`;
+
+    sessionStorage.setItem("backgroundColor", `${deg}`);
     //let tempDeg = deg + 120;
     //document.querySelector("body").style.color = `hsl(${tempDeg}deg 100% 50%)` // I don't like this
 }
