3b0c32afdc32f30d04780d4ecd8cc62ec0290f5b
shapeaca <shapeaca101@gmail.com>
2024-04-01 15:31:23 -0600

    JS - accountDisplay.js - Did the same thing but, loginButton also does the same thing as sign up button

--- startup/serverJavascript/loginButton.js
+++ startup/serverJavascript/loginButton.js
@@ -3,5 +3,10 @@ document.getElementById("loginButton").addEventListener("click", login);
 function login() {
     //fixme make signUp button only be able to be clicked once.
     console.log("login Button Pressed");
-
+    let username = document.getElementById("username").value;
+    if(username.length > 0) {
+        sessionStorage.setItem("username", `${username}`)
+        //Now Update Account Display on webpage
+        document.getElementById("accountDisplay").innerHTML = `User: ${username}`;
+    }
 }
--- startup/serverJavascript/signupButton.js
+++ startup/serverJavascript/signupButton.js
@@ -2,12 +2,8 @@ document.getElementById("signupButton").addEventListener("click", signUp);
 
 function signUp() {
     //fixme make signUp button only be able to be clicked once.
-    console.log("signUp Button Clicked");
-    //todo get input from input
     let username = document.getElementById("usernameSignup").value;
-    // console.log(username);
     if(username.length > 0) {
-        console.log("Checkpoint 1");
         sessionStorage.setItem("username", `${username}`)
         //Now Update Account Display on webpage
         document.getElementById("accountDisplay").innerHTML = `User: ${username}`;
